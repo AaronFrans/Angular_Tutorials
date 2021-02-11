1c976ea6f990ad6c9cd35e079f1127d0b0bcc115
AaronFrans <azdefatrhtws@gmail.com>
2021-02-11 13:52:00 +0100

    added styling to project

--- component-tutorial/src/app/server/server.component.ts
+++ component-tutorial/src/app/server/server.component.ts
@@ -3,6 +3,11 @@ import { Component } from "@angular/core";
 @Component({
     selector: `app-server`,
     templateUrl: './server.component.html',
+    styles: [`
+        h3{
+            color: green;
+        }
+       `]
 })
 export class ServerComponent {
 
