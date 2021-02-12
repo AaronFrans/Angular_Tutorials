73def861ccd02c1019ab87af96ccf40175329c3d
AaronFrans <azdefatrhtws@gmail.com>
2021-02-12 09:40:39 +0100

    Added ngClass

--- component-tutorial/src/app/server/server.component.ts
+++ component-tutorial/src/app/server/server.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styles: [
     `
-      h3 {
-        color: orange;
+      .online{
+        color: white;
       }
     `,
   ],
