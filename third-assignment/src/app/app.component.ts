dacf58f18112392174468b5fb721d8f7072924e9
AaronFrans <azdefatrhtws@gmail.com>
2021-02-12 10:15:52 +0100

    Third assignment finished

--- third-assignment/src/app/app.component.ts
+++ third-assignment/src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'bootstrap-styling-tutorial';
+  showData = false;
+  clickedDates = [];
+
+  onChangeDisplay() {
+    
+    this.clickedDates.push({
+      date: this.formatDate(Date.now()),
+      index: this.clickedDates.length,
+    });
+    this.showData = !this.showData;
+  }
+
+  formatDate(date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
 }
